53106737d511e091ebe88e7f54fee8b9a2418dd7
Mohamed ehab <64233832+Mohamedihab29592@users.noreply.github.com>
2022-06-29 06:03:22 +0200

    update and finish

---
+++ android/app/src/main/kotlin/com/example/algroriza_intership_210/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.algroriza_intership_210
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'onboarding.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+fontFamily: 'Schyler',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.white,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+        ),
+        scaffoldBackgroundColor: Colors.white,
+
+      ),
+      home: const OnBoarding(),
+    );
+  }
+}
+
